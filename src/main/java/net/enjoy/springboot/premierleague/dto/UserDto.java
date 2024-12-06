36df1e913d8a3671af1445684d928274f85c25dc
kevine <musonikevine620@gmail.com>
2024-12-06 07:04:28 +0200

    Initial commit

---
+++ src/main/java/net/enjoy/springboot/premierleague/dto/UserDto.java
@@ -0,0 +1,44 @@
+package net.enjoy.springboot.premierleague.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+import net.enjoy.springboot.premierleague.entity.User;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    @NotEmpty(message = "First name should not be empty")
+    private String firstName;
+    @NotEmpty(message = "Last name should not be empty")
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+//    @NotEmpty(message = "Password should not be empty")
+    private String password;
+    @NotEmpty(message = "Phone number should not be empty")
+    private String phoneNumber;
+    @Past(message = "Date of birth should be in the past")
+    private LocalDate dateOfBirth;
+
+    public static UserDto fromUser(User user){
+        return UserDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .phoneNumber(user.getPhoneNumber())
+                .dateOfBirth(user.getDateOfBirth())
+                .build();
+    }
+}
